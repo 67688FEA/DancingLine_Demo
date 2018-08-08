c79098797dc56cd2130f2f0b994cf5a1d0d36c20
67688FEA <359952820@qq.com>
2018-08-08 17:21:42 +0800

    update scene

--- Assets/02_Scripts/Game/GemController.cs
+++ Assets/02_Scripts/Game/GemController.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class GemController : MonoBehaviour {
 
+    public ParticleSystem particleSystem;
     private void OnTriggerEnter(Collider other)
     {
+        ParticleSystem instance = Instantiate(particleSystem);
+        instance.transform.position = gameObject.transform.position;
+        instance.Play();
         gameObject.SetActive(false);
     }
 
